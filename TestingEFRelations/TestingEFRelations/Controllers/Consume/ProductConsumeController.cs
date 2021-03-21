4f10b9ddc6cc7624f662f693afa72df62bb41ecd
labon54 <tarabishi-999@windowslive.com>
2021-03-21 15:55:22 +0300

    Consume GET request from products

--- TestingEFRelations/TestingEFRelations/Controllers/Consume/ProductConsumeController.cs
+++ TestingEFRelations/TestingEFRelations/Controllers/Consume/ProductConsumeController.cs
@@ -6,21 +6,48 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using TestingEFRelations.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace TestingEFRelations.Controllers.Consume
 {
     public class ProductConsumeController
     {
+        IHttpClientFactory _clientFactory;
 
-        public async Task<Product> Index(Product prod)
+        public ProductConsumeController(IHttpClientFactory clientFactory)
         {
-            var url = "https://localhost:44324/api/ProductApi";
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(url);
-            var result =  await response.Content.ReadFromJsonAsync<Product>();
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IEnumerable<Product>> Index()
+        
+        {
+            //var url = "https://localhost:44324/api/ProductApi";
+            //var httpClient = new HttpClient();
+            //var response = await httpClient.GetAsync(url);
+            //var result =  await response.Content.ReadAsStringAsync();
 
             //return result;
-            return result;
+
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "https://localhost:44324/api/ProductApi");
+
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.SendAsync(request);
+
+            //if (response.IsSuccessStatusCode)
+            //{
+            //var responseStream =await response.Content.ReadAsStreamAsync();
+            //IEnumerable<Product> product = await JsonSerializer.DeserializeAsync<IEnumerable<Product>>(responseStream);
+            //var product = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+
+            var product = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+            //}
+            return product;
 
         }
     }
