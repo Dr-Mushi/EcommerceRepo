8f183f2ad45738d1918ee51d04f2ac21d4b574ab
labon54 <tarabishi-999@windowslive.com>
2021-03-23 09:31:21 +0300

    Product Details API

--- TestingEFRelations/TestingEFRelations/Controllers/Consume/ProductConsumeController.cs
+++ TestingEFRelations/TestingEFRelations/Controllers/Consume/ProductConsumeController.cs
@@ -20,7 +20,8 @@ namespace TestingEFRelations.Controllers.Consume
     {
         IHttpClientFactory _clientFactory;
         private readonly IProductRepository _product;
-        IEnumerable<Product> product;
+        IEnumerable<Product> products;
+        Product product;
         string errorMessage;
 
         public ProductConsumeController(IHttpClientFactory clientFactory , IProductRepository product)
@@ -36,10 +37,51 @@ namespace TestingEFRelations.Controllers.Consume
 
             try
             {
-                product = await client.GetFromJsonAsync<IEnumerable<Product>>("ProductApi");
+                products = await client.GetFromJsonAsync<IEnumerable<Product>>("ProductApi");
                 errorMessage = null;
             }
             catch(Exception ex)
+            {
+                errorMessage = $"There was an error getting products : ({ex.Message})";
+            }
+            @ViewData["errorMessage"] = errorMessage;
+            return View(products);
+
+            //This is one way to do it.
+            //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,
+            //    "https://localhost:44324/api/ProductApis");
+
+            //HttpClient client = _clientFactory.CreateClient();
+
+            //HttpResponseMessage response = await client.SendAsync(request);
+
+            //if (response.IsSuccessStatusCode)
+            //{
+            //     product = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+            //    errorMessage = null;
+            //}
+            //else
+            //{
+            //    //product = new List<Product>();
+            //    errorMessage = "there was an error getting products.";
+            //}
+
+            //ViewData["errorMessage"] = errorMessage;
+            //return View(product);
+        }
+
+        
+        public async Task<ActionResult<Product>> Details(int id)
+        {
+            //this is another way to get the api
+            HttpClient client = _clientFactory.CreateClient("Ecommerce");
+
+            try
+            {
+                product = await client.GetFromJsonAsync<Product>($"ProductApi/{id}");
+                errorMessage = null;
+            }
+            catch (Exception ex)
             {
                 errorMessage = $"There was an error getting products : ({ex.Message})";
             }
@@ -82,6 +124,45 @@ namespace TestingEFRelations.Controllers.Consume
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(/*[Bind("ProductName,ProductDescription,ImageFile,SizeID,ProductQuantity,ProductPrice")]*/ Product product)
         {
+           
+            product.ImageFileName = new List<string>();
+            product.ImageFileExtension = new List<string>();
+            product.ImageFileData = new List<string>();
+            //for multiple images upload
+            for (int i  = 0; i < product.ImageFile.Count; i++)
+            {
+                product.ImageFileName.Add(product.ImageFile[i].FileName);
+                product.ImageFileExtension.Add(Path.GetExtension(product.ImageFile[i].FileName));
+                if (product.ImageFile[i].Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        await product.ImageFile[i].CopyToAsync(ms);
+                        byte[] fileBytes = ms.ToArray();
+                        product.ImageFileData.Add(Convert.ToBase64String(fileBytes));
+                    }
+                }
+            }
+
+
+
+            HttpClient client = _clientFactory.CreateClient("Ecommerce");
+
+            var response = await client.PostAsJsonAsync("ProductApi",product);
+
+            if (response.IsSuccessStatusCode)
+            {
+                errorMessage = "";
+            }
+            else
+            {
+                errorMessage = $"There was an error getting products : ({response.StatusCode})";
+            }
+
+            return RedirectToAction(nameof(Index)); ;
+
+
+
             ////this is another way to get the api
             //HttpClient client = _clientFactory.CreateClient("Ecommerce");
 
@@ -119,50 +200,6 @@ namespace TestingEFRelations.Controllers.Consume
             //        }
             //    }
             //}
-            product.ImageFileName = new List<string>();
-            product.ImageFileExtension = new List<string>();
-            product.ImageFileData = new List<string>();
-            //for multiple images upload
-            for (int i  = 0; i < product.ImageFile.Count; i++)
-            {
-                product.ImageFileName.Add(product.ImageFile[i].FileName);
-                product.ImageFileExtension.Add(Path.GetExtension(product.ImageFile[i].FileName));
-                if (product.ImageFile[i].Length > 0)
-                {
-                    using (var ms = new MemoryStream())
-                    {
-                        await product.ImageFile[i].CopyToAsync(ms);
-                        byte[] fileBytes = ms.ToArray();
-                        product.ImageFileData.Add(Convert.ToBase64String(fileBytes));
-                    }
-                }
-            }
-
-
-
-            HttpClient client = _clientFactory.CreateClient("Ecommerce");
-
-            var response = await client.PostAsJsonAsync("ProductApi",product);
-
-            
-            //var created = await response.Content.ReadFromJsonAsync<Product>();
-
-
-        
-
-
-
-
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                errorMessage = $"There was an error getting products : ({ex.Message})";
-            }
-
-            return View(nameof(Index));
 
             //HttpClient client = _clientFactory.CreateClient("Ecommerce");
             //var response = await client.PostAsJsonAsync("ProductApi", product);
@@ -181,6 +218,9 @@ namespace TestingEFRelations.Controllers.Consume
 
 
         }
+
+
+
 
 
 
